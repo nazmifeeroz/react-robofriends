cdfcbc1da66d78dcbf8071cb346a7db9da88cd9c
nazmi <nazmi@alpome.com>
2018-04-15 16:50:54 +0800

    use external json to render data

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import './App.css';
-import { robots } from './robots';
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => {return response.json()})
+        .then(newData => {this.setState({robots: newData})});
     }
 
     onSearchChange = (event) => {
