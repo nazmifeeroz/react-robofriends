4aed5bcfb6700f27f31e212bc9ba12e457dc1785
nazmi <nazmi@alpome.com>
2018-04-15 16:06:44 +0800

    finalised my own react app to search

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+    {
+        id :1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Bret@april.com',
+    },
+    {
+        id :2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@melissa.tv',
+    },
+    {
+        id :3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'Nathan@yesenia.net',
+    },
+    {
+        id :4,
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        email: 'Julianne.oconner@kory.org',
+    },
+]
